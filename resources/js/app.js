28c340ac6b232cba4dc6f434433c5622305ffbfd
Michel Cisneros <cisnerosllatasmichel@gmail.com>
2025-05-03 16:45:29 -0500

    cambios Actulziados

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
 import './bootstrap';
-import './applogin.js';
+import './demo/scripts.js'
+import './demo/datatables-simple-demo.js';
+import './demo/chart-area-demo.js';
+import './demo/chart-bar-demo.js';
+import './demo/chart-pie-demo.js';
+import './demo/datatables-demo.js';
+// import './applogin.js';
